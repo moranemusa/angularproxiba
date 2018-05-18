82940990350fdf7c2a4f7658e037a077cb5aaa83
morane <morane.musa@gmail.com>
2018-05-18 15:00:21 +0200

    creation service et component

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { ClientListComponent } from './client-list/client-list.component';
+import { ClientFormComponent } from './client-form/client-form.component';
+
+
+const route:Routes=[
+  { path: 'listeClients', component: ClientListComponent },
+  { path: 'editClient/:clientId', component: ClientFormComponent },
+  { path: 'newClient', component: ClientFormComponent },
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, RouterModule.forRoot(route)
   ],
-  declarations: []
+  declarations: [ClientListComponent, ClientFormComponent]
 })
 export class ClientModule { }
---
+++ src/app/client/client.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ClientService {
+
+  constructor() { }
+
+  loadClients(){
+
+  }
+  loadClient(){
+
+  }
+  saveClient(){
+
+  }
+  deleteClient(){
+    
+  }
+}
